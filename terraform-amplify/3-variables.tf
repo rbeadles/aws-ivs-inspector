0156dbe010587dee4481e9b61c425f2fc2c1624f
Sathia Saithanya <sathimal@amazon.com>
2024-05-03 02:53:51 +0800

    auto build web app 3

--- terraform-amplify/3-variables.tf
+++ terraform-amplify/3-variables.tf
@@ -17,6 +17,25 @@ variable "environment" {
 }
 
 variable "token" {
-  type      = string
-  sensitive = true
+  type        = string
+  description = "github token to connect github repo"
+  sensitive   = true
+}
+
+variable "repository" {
+  type        = string
+  description = "AWS IVS Inspector Web Application Repo"
+  default     = "https://github.com/sathia-s/aws-ivs-inspector-web-public"
+}
+
+variable "branch_name" {
+  type        = string
+  description = "AWS IVS Inspector IVS Branch"
+  default     = "ivs"
+}
+
+variable "domain_name" {
+  type        = string
+  default     = "awsivsinspector.com"
+  description = "AWS Amplify Domain Name"
 }
