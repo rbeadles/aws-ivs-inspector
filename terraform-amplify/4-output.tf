4a4b845673937824ae9c1ddf68aeffc24c788a3c
Sathia Saithanya <sathimal@amazon.com>
2024-05-03 20:31:38 +0800

    auto build web app 9

--- terraform-amplify/4-output.tf
+++ terraform-amplify/4-output.tf
@@ -1,5 +1,5 @@
-output "amplify_app_url" {
-  value = aws_amplify_domain_association.domain_association.domain_name
+output "amplify_app_id" {
+  value = aws_amplify_app.amplify_app.id
 }
 
 output "amplify_app_arn" {
