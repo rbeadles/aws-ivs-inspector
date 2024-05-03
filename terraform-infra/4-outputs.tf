96ec6694db8ab7d6be3046885235cacf0aff7bf7
Sathia Saithanya <sathimal@amazon.com>
2024-05-04 00:36:21 +0800

    auto build web app 9

--- terraform-infra/4-outputs.tf
+++ terraform-infra/4-outputs.tf
@@ -1,9 +1,17 @@
 output "api_ids" {
   value = {
-    "${var.region}" : {
+    "${var.region}" : jsonencode({
       rest : module.aws-infra.rest_api_id,
       wss_get_session_events : module.aws-infra.wss_get_session_events_api_id,
       wss_get_live_streams : module.aws-infra.wss_get_live_streams_api_id
-    }
+    })
   }
 }
+
+# locals {
+#   api_ids_jsonencode = jsonencode({
+#     rest : module.aws-infra.rest_api_id,
+#     wss_get_session_events : module.aws-infra.wss_get_session_events_api_id,
+#     wss_get_live_streams : module.aws-infra.wss_get_live_streams_api_id
+#   })
+# }
