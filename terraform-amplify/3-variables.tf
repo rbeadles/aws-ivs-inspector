83ff05708bc2171d28295822ef40c9a8d2ba5c9a
Sathia Saithanya <sathimal@amazon.com>
2024-05-04 22:02:22 +0800

    auto build web app 9

--- terraform-amplify/3-variables.tf
+++ terraform-amplify/3-variables.tf
@@ -19,7 +19,7 @@ variable "environment" {
 variable "repository" {
   type        = string
   description = "Web Application Repo"
-  default     = "https://github.com/sathia-s/aws-ivs-inspector/web-application"
+  default     = "https://github.com/sathia-s/aws-ivs-inspector"
 }
 
 variable "token" {
