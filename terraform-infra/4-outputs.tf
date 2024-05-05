09ba3fab61159eb5537edffe6bffbe3a12c6612e
Sathia Saithanya <sathimal@amazon.com>
2024-05-05 17:28:29 +0800

    dynamic json build

--- terraform-infra/4-outputs.tf
+++ terraform-infra/4-outputs.tf
@@ -2,8 +2,8 @@ output "api_ids" {
   value = {
     "${var.region}" : {
       rest : module.aws-infra.rest_api_id,
-      wss_get_session_events : module.aws-infra.wss_get_session_events_api_id,
-      wss_get_live_streams : module.aws-infra.wss_get_live_streams_api_id
+      get_session_events : module.aws-infra.get_session_events_api_id,
+      get_live_streams : module.aws-infra.get_live_streams_api_id
     }
   }
 }
--- terraform-infra/modules/aws-infra/output.tf
+++ terraform-infra/modules/aws-infra/output.tf
@@ -3,10 +3,10 @@ output "rest_api_id" {
   value = aws_api_gateway_deployment.deployment.rest_api_id
 }
 
-output "wss_get_session_events_api_id" {
+output "get_session_events_api_id" {
   value = aws_apigatewayv2_stage.stage["get-session-events"].api_id
 }
 
-output "wss_get_live_streams_api_id" {
+output "get_live_streams_api_id" {
   value = aws_apigatewayv2_stage.stage["get-live-streams"].api_id
 }
