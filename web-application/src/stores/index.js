8500d76eb99c1ce8de7098dd465a2c5937d3e197
Sathia Saithanya <sathimal@amazon.com>
2024-05-08 22:54:43 +0800

    Adding User Auth using Cognito 10

--- web-application/src/stores/index.js
+++ web-application/src/stores/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { store } from "quasar/wrappers";
 import { createPinia } from "pinia";
 
@@ -7,25 +6,3 @@ export default store((/* { ssrContext } */) => {
 
   return pinia;
 });
-=======
-import { store } from 'quasar/wrappers'
-import { createPinia } from 'pinia'
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export default store((/* { ssrContext } */) => {
-  const pinia = createPinia()
-
-  // You can add Pinia plugins here
-  // pinia.use(SomePiniaPlugin)
-
-  return pinia
-})
->>>>>>> refs/remotes/origin/dev
