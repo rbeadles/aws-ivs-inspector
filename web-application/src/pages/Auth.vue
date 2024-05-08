5afdf56cf6e12ef52dba44400f79b28af7d9251b
Sathia Saithanya <sathimal@amazon.com>
2024-05-08 22:57:47 +0800

    Adding User Auth using Cognito 11

---
+++ web-application/src/pages/Auth.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="col body-spacing">
+    <authenticator
+      initial-state="signUp"
+      :form-fields="formFields"
+      :sign-up-attributes="signUpAttributes"
+    >
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </authenticator>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { useRoute } from "vue-router";
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+export default defineComponent({
+  name: "UserAuthentication",
+
+  components: { Authenticator },
+
+  setup() {
+    const formFields = {
+      signUp: {
+        given_name: {
+          order: 1,
+        },
+        family_name: {
+          order: 2,
+        },
+        email: {
+          order: 3,
+        },
+        password: {
+          order: 4,
+        },
+        confirm_password: {
+          order: 5,
+        },
+      },
+    };
+    return {
+      formFields,
+    };
+  },
+});
+</script>
