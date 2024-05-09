6c3d3c0386de0955896e156c6e1d8adea3a082a4
Sathia Saithanya <sathimal@amazon.com>
2024-05-09 14:58:53 +0800

    setting rest api throttle limits

--- terraform-infra/modules/aws-infra/1-apigateway-rest.tf
+++ terraform-infra/modules/aws-infra/1-apigateway-rest.tf
@@ -91,6 +91,17 @@ resource "aws_api_gateway_stage" "stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   stage_name    = var.environment
+
+}
+
+resource "aws_api_gateway_method_settings" "get_method_settings" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = aws_api_gateway_stage.stage.stage_name
+  method_path = "*/*"
+  settings {
+    throttling_burst_limit = 4999
+    throttling_rate_limit  = 9999
+  }
 }
 
 resource "aws_api_gateway_usage_plan" "usage_plan" {
