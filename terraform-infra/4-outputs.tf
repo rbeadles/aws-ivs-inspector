2297bd1af6ed3873b28000f6531dfb1516714272
Sathia Saithanya <sathimal@amazon.com>
2024-05-11 02:09:16 +0800

    improving env vars 3

--- terraform-infra/4-outputs.tf
+++ terraform-infra/4-outputs.tf
@@ -1,11 +1,11 @@
 output "api_ids" {
-  value = {
-    "${var.region}" : jsonencode({
+  value = jsonencode({
+    "${var.region}" : {
       rest : module.aws-infra.rest_api_id,
       get_session_events : module.aws-infra.get_session_events_api_id,
       get_live_streams : module.aws-infra.get_live_streams_api_id
-    })
-  }
+    }
+  })
 }
 
 # locals {
