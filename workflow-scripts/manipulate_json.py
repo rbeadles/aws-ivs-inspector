9b47d70b3bb8d70c9b1b0b871fb7f21974db6cf0
Sathia Saithanya <sathimal@amazon.com>
2024-05-12 02:04:36 +0800

    improving env vars 3

--- workflow-scripts/manipulate_json.py
+++ workflow-scripts/manipulate_json.py
@@ -1,7 +1,7 @@
 import sys, json;
 
-print("existing env vars: ", sys.argv[0])
-print("api_values: ", sys.argv[1])
-print("api_values json: ", json.load(sys.argv[1]))
+print("existing env vars: ", sys.argv[1])
+print("api_values: ", sys.argv[2])
+print("api_values json: ", json.loads(sys.argv[2]))
 
 # print(json.load(sys.stdin))
