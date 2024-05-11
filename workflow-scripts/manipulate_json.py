3deea18c647e4a7f690212282299e98e801c0579
Sathia Saithanya <sathimal@amazon.com>
2024-05-12 04:14:08 +0800

    improving env vars 3

--- workflow-scripts/manipulate_json.py
+++ workflow-scripts/manipulate_json.py
@@ -8,9 +8,10 @@ api_values = sys.argv[3]
 # print("existing env vars json: ", json.loads(existing_env_vars))
 # print("existing env vars json type: ", type(json.loads(existing_env_vars)))
 # print("existing env vars type: ", type(existing_env_vars))
-# print("api_values: ", api_values)
-# print("api_values json: ", json.loads(api_values))
-# print("api_values type: ", type(json.loads(api_values)))
+print("api_values: ", api_values)
+print("api_values type: ", type(api_values))
+print("api_values json: ", json.loads(api_values))
+print("api_values json type: ", type(json.loads(api_values)))
 # print("region:", region)
 # print("region type:", type(region))
  
