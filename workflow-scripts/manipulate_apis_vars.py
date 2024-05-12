629e04131288d403d013082bf2fdc792519bd2c6
Sathia Saithanya <sathimal@amazon.com>
2024-05-13 04:45:48 +0800

    minor improvements

--- workflow-scripts/manipulate_apis_vars.py
+++ workflow-scripts/manipulate_apis_vars.py
@@ -3,23 +3,11 @@ import sys, json;
 existing_env_vars = json.loads(sys.argv[1])
 region = sys.argv[2]
 api_values = json.loads(sys.argv[3])
-
-# print("existing env vars: ", existing_env_vars)
-# print("existing env vars json: ", json.loads(existing_env_vars))
-# print("existing env vars json type: ", type(json.loads(existing_env_vars)))
-# print("existing env vars type: ", type(existing_env_vars))
-# print("api_values: ", api_values)
-# print("api_values json: ", json.loads(api_values))
-# print("api_values type: ", type(json.loads(api_values)))
-# print("region:", region)
-# print("region type:", type(region))
  
-# compiled_env_vars.update(existing_env_vars)
-# print(compiled_env_vars)
 existing_env_vars[region] = api_values
 region_keys = list(map(lambda x: x.replace("VITE_API_", ""), [k for k in existing_env_vars.keys() if "VITE_API_" in k]))
 
-existing_env_vars["VITE_REGIONS"] = ",".join(region_keys)
+existing_env_vars["VITE_IVS_REGIONS"] = ",".join(region_keys)
 
 print(json.dumps(existing_env_vars))
 
