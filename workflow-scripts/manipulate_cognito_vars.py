e52f744ff98d3201df049c9a29d36e1931734082
Sathia Saithanya <sathimal@amazon.com>
2024-05-13 15:18:33 +0800

    VITE_REGIONS to VITE_IVS_REGIONS 5

--- workflow-scripts/manipulate_cognito_vars.py
+++ workflow-scripts/manipulate_cognito_vars.py
@@ -14,13 +14,14 @@ def assign_cognito_vars():
         existing_env_vars[key] = value
 
 
-if existing_env_vars == "None":
+if existing_env_vars == None:
     print(type(existing_env_vars))
     existing_env_vars = {}
     print(type(existing_env_vars))
     # assign_cognito_vars()
     
 else:
+    print(type(existing_env_vars))
     assign_cognito_vars()
 
 print(json.dumps(existing_env_vars))
