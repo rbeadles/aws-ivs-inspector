a14b1e8650bdd95be90d6d8c2500953272b22562
Sathia Saithanya <sathimal@amazon.com>
2024-05-13 14:53:54 +0800

    VITE_REGIONS to VITE_IVS_REGIONS 5

--- workflow-scripts/manipulate_cognito_vars.py
+++ workflow-scripts/manipulate_cognito_vars.py
@@ -4,12 +4,13 @@ existing_env_vars = json.loads(sys.argv[1])
 cognito_vars = json.loads(sys.argv[2])
 
 print("existing_env_vars: ", existing_env_vars)
+print("existing_env_vars type: ", type(existing_env_vars))
 print("cognito_vars: ", cognito_vars)
 print("cognito_vars type: ", type(cognito_vars))
 
 for key, value in cognito_vars.items():
     print(key, value)
-    # existing_env_vars[key] = value
+    existing_env_vars[key] = value
 
 print(json.dumps(existing_env_vars))
 
