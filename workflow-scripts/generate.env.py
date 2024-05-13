2714890f2c232daccf54da2a461952b9fbbff038
Sathia Saithanya <sathimal@amazon.com>
2024-05-13 14:50:57 +0800

    VITE_REGIONS to VITE_IVS_REGIONS 5

--- workflow-scripts/generate.env.py
+++ workflow-scripts/generate.env.py
@@ -7,8 +7,9 @@ existing_env_vars = json.loads(sys.argv[1])
 env_file_path = Path("./.env")
 env_file_path.touch(mode=0o600, exist_ok=False)
 
-for k,v in existing_env_vars.items():
-    if not k.startswith("VITE_"):
-        set_key(dotenv_path=env_file_path, key_to_set=f"VITE_{k}", value_to_set=v)
-    else:
-        set_key(dotenv_path=env_file_path, key_to_set=k, value_to_set=v)
+if len(existing_env_vars):
+    for k,v in existing_env_vars.items():
+        if not k.startswith("VITE_"):
+            set_key(dotenv_path=env_file_path, key_to_set=f"VITE_{k}", value_to_set=v)
+        else:
+            set_key(dotenv_path=env_file_path, key_to_set=k, value_to_set=v)
